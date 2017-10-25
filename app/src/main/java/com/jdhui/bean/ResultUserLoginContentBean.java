6d60678ad5cdff3a961e22d318d66e5bec8f2475
lvhl3 <lvhl3@ziroom.com>
2017-10-25 10:10:00 +0800

    添加 注释

--- app/src/main/java/com/jdhui/bean/ResultUserLoginContentBean.java
+++ app/src/main/java/com/jdhui/bean/ResultUserLoginContentBean.java
@@ -4,15 +4,16 @@ import com.jdhui.mould.types.IMouldType;
 
 public class ResultUserLoginContentBean implements IMouldType {
 
-	private String flag;
-	private String message;
-	private String userId;
-	private String nickName;
-	private String mobile;
-	private String questionnaireRecordFlag;
-	private String qualifiedInvestorFlag;
-	private String totalAmountFlag;
+	private String flag; // true/false,true为登录成功，false为登录失败
+	private String message; // 登录成功或失败信息
+	private String userId; // 用户ID
+	private String nickName; // 用户昵称
+	private String mobile; // 手机号
+	private String questionnaireRecordFlag; // 判断是否答题false:未答题 true:已答题
+	private String qualifiedInvestorFlag; // 是否做过合格投资者判定 false:未做合格投资者判定 true:做过合格投资者判定
+	private String totalAmountFlag; // 账户资产是否大于300万false:判定为非合格投资者 true:判定为合格投资者
 	private String token;
+	private String realName; // 真实姓名
 
 	// public UserLoginResultBean(String flag, String message){
 	// setFlag(flag);
@@ -102,5 +103,13 @@ public class ResultUserLoginContentBean implements IMouldType {
 
 	public void setTotalAmountFlag(String totalAmountFlag) {
 		this.totalAmountFlag = totalAmountFlag;
+	}
+
+	public String getRealName() {
+		return realName;
+	}
+
+	public void setRealName(String realName) {
+		this.realName = realName;
 	}
 }
